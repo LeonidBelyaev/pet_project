2f005ec9d6b2d5ed166ca7357f674015354dc52b
1 <alanmiolat@gmail.com>
2025-02-01 18:15:07 +0300

    DELETE and PATCH added

--- main/main.go
+++ main/main.go
@@ -34,11 +34,44 @@ func TaskHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var req requestBody
+		vars := mux.Vars(r)
+		id := vars["id"]
+		json.NewDecoder(r.Body).Decode(&req)
+
+		var message Message
+		DB.First(&message, id)
+		if req.Message != "" {
+			message.Task = req.Message
+		}
+		if req.Message != "" {
+			message.IsDone = true
+		}
+		DB.Save(&message)
+		json.NewEncoder(w).Encode(message)
+	}
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodDelete {
+		var req requestBody
+		vars := mux.Vars(r)
+		id := vars["id"]
+		json.NewDecoder(r.Body).Decode(&req)
+		var message Message
+		DB.Delete(&message, id)
+	}
+}
+
 func main() {
 	InitDB()
 	DB.AutoMigrate(&Message{})
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/task", TaskHandler).Methods("POST")
+	router.HandleFunc("/api/task/{id}", PatchHandler).Methods("PATCH")
+	router.HandleFunc("/api/task/{id}", DeleteHandler).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
